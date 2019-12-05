06412060bde674c27f07c29e853f17a067e3b72c
viva <vivekaloganathan37@gmail.com>
2019-12-05 21:13:58 +0530

    With the function

--- index.js
+++ index.js
@@ -5,25 +5,43 @@ app.get('/',function (req,res){
     res.send ("Hello World");
 });
 
-// app.get('/user-identifier/:id',function(req,res){
-//     if (req.params.id == 1234) {
-//         res.send("siva");
-//     } else {
-//         res.send("priya");
-//     }
-// });
-
-app.get('/:id',function (req,res){
-    let username = "hello " + req.params.id;
-    let name = "hello " + req.query.name;
-    res.send(name);
+app.get('/user-identifier/:id',function(req,res){
+    if (req.params.id == 1234) {
+        res.send("siva");
+    } else {
+        res.send("priya");
+    }
 });
 
+app.get('/:id',function (req,res){
+    let html = "hello " + req.params.id + "<br>";
+    html += "hello " + req.query.name;
+    res.send(html);
+});
+
+
+/*
+ promise async and await with setTimeInterval
+*/
+
+async function calc(){
+    c= await add(a,b);
+}
+console.log ("Started");
+
+function add(a,b){
+
+    return new promise (function (resolve,reject) {
+
+setInterval(function(){
+    resolve (a + b);
+    console.log("waiting");
+},2000);
+    });
+
+}
+
+
+console.log ("Ended");
+
 app.listen(8080);
-// function add(a, b) {
-//     return a + b;
-// }
-
-// let c= add (0,10);
-
-// console.log("C value is " +c);
